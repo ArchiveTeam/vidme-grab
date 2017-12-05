38e1d46471eac69dd62aebdd0f519368080e5a9f
Arkiver2 <Arkiver@hotmail.com>
2017-12-05 18:28:13 +0100

    initial

---
+++ pipeline.py
@@ -0,0 +1,346 @@
+# encoding=utf8
+import datetime
+from distutils.version import StrictVersion
+import hashlib
+import os.path
+import random
+from seesaw.config import realize, NumberConfigValue
+from seesaw.externalprocess import ExternalProcess
+from seesaw.item import ItemInterpolation, ItemValue
+from seesaw.task import SimpleTask, LimitConcurrent
+from seesaw.tracker import GetItemFromTracker, PrepareStatsForTracker, \
+    UploadWithTracker, SendDoneToTracker
+import shutil
+import socket
+import subprocess
+import sys
+import time
+import string
+
+sys.path.insert(0, os.getcwd())
+
+import warcio
+from warcio.archiveiterator import ArchiveIterator
+from warcio.warcwriter import WARCWriter
+
+if not warcio.__file__ == os.path.join(os.getcwd(), 'warcio', '__init__.pyc'):
+    print('Warcio was not imported correctly.')
+    print('Location: ' + warcio.__file__ + '.')
+    sys.exit(1)
+
+import seesaw
+from seesaw.externalprocess import WgetDownload
+from seesaw.pipeline import Pipeline
+from seesaw.project import Project
+from seesaw.util import find_executable
+
+
+# check the seesaw version
+if StrictVersion(seesaw.__version__) < StrictVersion("0.8.5"):
+    raise Exception("This pipeline needs seesaw version 0.8.5 or higher.")
+
+
+###########################################################################
+# Find a useful Wget+Lua executable.
+#
+# WGET_LUA will be set to the first path that
+# 1. does not crash with --version, and
+# 2. prints the required version string
+WGET_LUA = find_executable(
+    "Wget+Lua",
+    ["GNU Wget 1.14.lua.20130523-9a5c", "GNU Wget 1.14.lua.20160530-955376b"],
+    [
+        "./wget-lua",
+        "./wget-lua-warrior",
+        "./wget-lua-local",
+        "../wget-lua",
+        "../../wget-lua",
+        "/home/warrior/wget-lua",
+        "/usr/bin/wget-lua"
+    ]
+)
+
+if not WGET_LUA:
+    raise Exception("No usable Wget+Lua found.")
+
+
+###########################################################################
+# The version number of this pipeline definition.
+#
+# Update this each time you make a non-cosmetic change.
+# It will be added to the WARC files and reported to the tracker.
+VERSION = "20171205.01"
+USER_AGENT = 'ArchiveTeam'
+TRACKER_ID = 'vidme'
+TRACKER_HOST = 'tracker.archiveteam.org'
+
+
+###########################################################################
+# This section defines project-specific tasks.
+#
+# Simple tasks (tasks that do not need any concurrency) are based on the
+# SimpleTask class and have a process(item) method that is called for
+# each item.
+class CheckIP(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, "CheckIP")
+        self._counter = 0
+
+    def process(self, item):
+        # NEW for 2014! Check if we are behind firewall/proxy
+
+        if self._counter <= 0:
+            item.log_output('Checking IP address.')
+            ip_set = set()
+
+            ip_set.add(socket.gethostbyname('twitter.com'))
+            ip_set.add(socket.gethostbyname('facebook.com'))
+            ip_set.add(socket.gethostbyname('youtube.com'))
+            ip_set.add(socket.gethostbyname('microsoft.com'))
+            ip_set.add(socket.gethostbyname('icanhas.cheezburger.com'))
+            ip_set.add(socket.gethostbyname('archiveteam.org'))
+
+            if len(ip_set) != 6:
+                item.log_output('Got IP addresses: {0}'.format(ip_set))
+                item.log_output(
+                    'Are you behind a firewall/proxy? That is a big no-no!')
+                raise Exception(
+                    'Are you behind a firewall/proxy? That is a big no-no!')
+
+        # Check only occasionally
+        if self._counter <= 0:
+            self._counter = 10
+        else:
+            self._counter -= 1
+
+
+class PrepareDirectories(SimpleTask):
+    def __init__(self, warc_prefix):
+        SimpleTask.__init__(self, "PrepareDirectories")
+        self.warc_prefix = warc_prefix
+
+    def process(self, item):
+        item_name = item["item_name"]
+        escaped_item_name = item_name.replace(':', '_').replace('/', '_').replace('~', '_')
+        dirname = "/".join((item["data_dir"], escaped_item_name))
+
+        if os.path.isdir(dirname):
+            shutil.rmtree(dirname)
+
+        os.makedirs(dirname)
+
+        item["item_dir"] = dirname
+        item["warc_file_base"] = "%s-%s-%s" % (self.warc_prefix, escaped_item_name[:50],
+            time.strftime("%Y%m%d-%H%M%S"))
+
+        open("%(item_dir)s/%(warc_file_base)s.warc.gz" % item, "w").close()
+        open("%(item_dir)s/%(warc_file_base)s_data.txt" % item, "w").close()
+
+
+class MoveFiles(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, "MoveFiles")
+
+    def process(self, item):
+        # NEW for 2014! Check if wget was compiled with zlib support
+        if os.path.exists("%(item_dir)s/%(warc_file_base)s.warc" % item):
+            raise Exception('Please compile wget with zlib support!')
+
+        os.rename("%(item_dir)s/%(warc_file_base)s_data.txt" % item,
+              "%(data_dir)s/%(warc_file_base)s_data.txt" % item)
+
+        shutil.rmtree("%(item_dir)s" % item)
+
+
+class DeduplicateWarc(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, "DeduplicateWarc")
+        self.records = {}
+
+    def create_revisit_record(self, writer, record, data):
+        warc_headers = record.rec_headers
+        warc_headers.add_header('WARC-Refers-To', data['id'])
+        warc_headers.replace_header('WARC-Refers-To-Date', data['date'])
+        warc_headers.replace_header('WARC-Refers-To-Target-URI', data['url'])
+        warc_headers.replace_header('WARC-Type', 'revisit')
+        warc_headers.replace_header('WARC-Truncated', 'length')
+        warc_headers.replace_header('WARC-Profile', 'http://netpreserve.org/warc/1.1/revisit/identical-payload-digest')
+        warc_headers.remove_header('WARC-Block-Digest')
+        warc_headers.remove_header('Content-Length')
+        return writer.create_warc_record(
+            record.rec_headers.get_header('WARC-Target-URI'),
+            'revisit',
+            warc_headers=warc_headers,
+            http_headers=record.http_headers
+        )
+
+    def process_record(self, writer, record, out):
+        if record.rec_headers.get_header('WARC-Type') == 'response':
+            record_url = record.rec_headers.get_header('WARC-Target-URI')
+            record_digest = record.rec_headers.get_header('WARC-Payload-Digest')
+            record_date = record.rec_headers.get_header('WARC-Date')
+            record_id = record.rec_headers.get_header('WARC-Record-ID')
+            print('Deduplicating digest ' + record_digest + ', url ' + record_url)
+
+            if record_digest in self.records:
+                print('Found duplicate, writing revisit record.')
+                return self.create_revisit_record(writer, record,
+                                                  self.records[record_digest])
+            else:
+                self.records[record_digest] = {
+                    'url': record_url,
+                    'date': record_date,
+                    'id': record_id
+                }
+                return record
+        else:
+            return record
+
+    def process(self, item):
+        filename_in = '%(item_dir)s/%(warc_file_base)s.warc.gz' % item
+        filename_out = '%(data_dir)s/%(warc_file_base)s.warc.gz' % item
+
+        with open(filename_in, 'rb') as file_in:
+            with open(filename_out, 'wb') as file_out:
+                writer = WARCWriter(filebuf=file_out, gzip=True)
+                for record in ArchiveIterator(file_in):
+                    writer.write_record(self.process_record(writer, record,
+                                                            filename_out))
+
+def get_hash(filename):
+    with open(filename, 'rb') as in_file:
+        return hashlib.sha1(in_file.read()).hexdigest()
+
+CWD = os.getcwd()
+PIPELINE_SHA1 = get_hash(os.path.join(CWD, 'pipeline.py'))
+LUA_SHA1 = get_hash(os.path.join(CWD, 'vidme.lua'))
+
+def stats_id_function(item):
+    # NEW for 2014! Some accountability hashes and stats.
+    d = {
+        'pipeline_hash': PIPELINE_SHA1,
+        'lua_hash': LUA_SHA1,
+        'python_version': sys.version,
+    }
+
+    return d
+
+
+class WgetArgs(object):
+    def realize(self, item):
+        wget_args = [
+            WGET_LUA,
+            "-U", USER_AGENT,
+            "-nv",
+            "--no-cookies",
+            "--lua-script", "vidme.lua",
+            "-o", ItemInterpolation("%(item_dir)s/wget.log"),
+            "--no-check-certificate",
+            "--output-document", ItemInterpolation("%(item_dir)s/wget.tmp"),
+            "--truncate-output",
+            "-e", "robots=off",
+            "--rotate-dns",
+            "--recursive", "--level=inf",
+            "--no-parent",
+            "--page-requisites",
+            "--timeout", "30",
+            "--tries", "inf",
+            "--domains", "vid.me",
+            "--span-hosts",
+            "--waitretry", "30",
+            "--warc-file", ItemInterpolation("%(item_dir)s/%(warc_file_base)s"),
+            "--warc-header", "operator: Archive Team",
+            "--warc-header", "vidme-dld-script-version: " + VERSION,
+            "--warc-header", ItemInterpolation("vidme-item: %(item_name)s"),
+        ]
+
+        item_name = item['item_name']
+        assert ':' in item_name
+        item_type, item_value = item_name.split(':', 1)
+
+        item['item_type'] = item_type
+        item['item_value'] = item_value
+
+        if item_type == 'video':
+            wget_args.extend(['--warc-header', 'vidme-video-id: {i}'.format(i=item_value)])
+            wget_args.append('https://api.vid.me/video/{i}'.format(i=item_value))
+            wget_args.append('https://api.vid.me/video/{i}/upnext'.format(i=item_value))
+            wget_args.append('https://api.vid.me/video/{i}/likes?offset=0&limit=10'.format(i=item_value))
+            wget_args.append('https://api.vid.me/video/{i}/comments?offsetAtParentLevel=true&order=score&offset=0&limit=20'.format(i=item_value))
+        else:
+            raise Exception('Unknown item')
+
+        if 'bind_address' in globals():
+            wget_args.extend(['--bind-address', globals()['bind_address']])
+            print('')
+            print('*** Wget will bind address at {0} ***'.format(
+                globals()['bind_address']))
+            print('')
+
+        return realize(wget_args, item)
+
+###########################################################################
+# Initialize the project.
+#
+# This will be shown in the warrior management panel. The logo should not
+# be too big. The deadline is optional.
+project = Project(
+    title = "vidme",
+    project_html = """
+    <img class="project-logo" alt="logo" src="http://archiveteam.org/images/e/e2/Vidme_logo.png" />
+    <h2>vid.me <span class="links"><a href="https://vid.me/">Website</a> &middot; <a href="http://tracker.archiveteam.org/vidme/">Leaderboard</a></span></h2>
+    """,
+    utc_deadline = datetime.datetime(2017, 12, 15, 23, 59, 0)
+)
+
+pipeline = Pipeline(
+    CheckIP(),
+    GetItemFromTracker("http://%s/%s" % (TRACKER_HOST, TRACKER_ID), downloader,
+        VERSION),
+    PrepareDirectories(warc_prefix="vidme"),
+    WgetDownload(
+        WgetArgs(),
+        max_tries=2,
+        accept_on_exit_code=[0, 4, 8],
+        env={
+            "item_dir": ItemValue("item_dir"),
+            "item_value": ItemValue("item_value"),
+            "item_type": ItemValue("item_type"),
+            "warc_file_base": ItemValue("warc_file_base"),
+        }
+    ),
+    DeduplicateWarc(),
+    PrepareStatsForTracker(
+        defaults={"downloader": downloader, "version": VERSION},
+        file_groups={
+            "data": [
+                ItemInterpolation("%(item_dir)s/%(warc_file_base)s.warc.gz")
+            ]
+        },
+        id_function=stats_id_function,
+    ),
+    MoveFiles(),
+    LimitConcurrent(NumberConfigValue(min=1, max=20, default="20",
+        name="shared:rsync_threads", title="Rsync threads",
+        description="The maximum number of concurrent uploads."),
+        UploadWithTracker(
+            "http://%s/%s" % (TRACKER_HOST, TRACKER_ID),
+            downloader=downloader,
+            version=VERSION,
+            files=[
+                ItemInterpolation("%(data_dir)s/%(warc_file_base)s_data.txt"),
+                ItemInterpolation("%(data_dir)s/%(warc_file_base)s.warc.gz")
+            ],
+            rsync_target_source_path=ItemInterpolation("%(data_dir)s/"),
+            rsync_extra_args=[
+                "--recursive",
+                "--partial",
+                "--partial-dir", ".rsync-tmp",
+            ]
+        ),
+    ),
+    SendDoneToTracker(
+        tracker_url="http://%s/%s" % (TRACKER_HOST, TRACKER_ID),
+        stats=ItemValue("stats")
+    )
+)
---
+++ vidme.lua
@@ -0,0 +1,294 @@
+dofile("urlcode.lua")
+dofile("table_show.lua")
+JSON = (loadfile "JSON.lua")()
+
+local item_type = os.getenv('item_type')
+local item_value = string.lower(os.getenv('item_value'))
+local item_dir = os.getenv('item_dir')
+local warc_file_base = os.getenv('warc_file_base')
+
+local url_count = 0
+local tries = 0
+local downloaded = {}
+local addedtolist = {}
+local abortgrab = false
+
+local identifiers = {}
+
+local disco_users = {}
+local disco_tags = {}
+
+identifiers[item_value] = true
+
+for ignore in io.open("ignore-list", "r"):lines() do
+  downloaded[ignore] = true
+end
+
+load_json_file = function(file)
+  if file then
+    return JSON:decode(file)
+  else
+    return nil
+  end
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+allowed = function(url, parenturl)
+  if string.match(url, "'+")
+     or string.match(url, "[<>\\{}]")
+     or string.match(url, "//$")
+     or string.match(url, "locale%.lang")
+     or string.match(url, "subscribe$")
+     or not (string.match(url, "^https?://[^/]*vid%.me")
+      or string.match(url, "^https?://[^/]*cloudfront%.net")) then
+    return false
+  end
+
+  for s in string.gmatch(url, "([0-9a-zA-Z]+)") do
+    if identifiers[s] == true then
+      return true
+    end
+  end
+
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+  local html = urlpos["link_expect_html"]
+
+  if (downloaded[url] ~= true and addedtolist[url] ~= true)
+     and (allowed(url, parent["url"]) or html == 0) then
+    addedtolist[url] = true
+    return true
+  end
+
+  return false
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  local html = nil
+
+  downloaded[url] = true
+  
+  local function check(urla)
+    local origurl = url
+    local url = string.match(urla, "^([^#]+)")
+    local url_ = string.gsub(url, "&amp;", "&")
+    if (downloaded[url_] ~= true and addedtolist[url_] ~= true)
+       and allowed(url_, origurl) then
+      table.insert(urls, { url=url_ })
+      addedtolist[url_] = true
+      addedtolist[url] = true
+    end
+  end
+
+  local function checknewurl(newurl)
+    if string.match(newurl, "^https?:////") then
+      check(string.gsub(newurl, ":////", "://"))
+    elseif string.match(newurl, "^https?://") then
+      check(newurl)
+    elseif string.match(newurl, "^https?:\\/\\?/") then
+      check(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^\\/\\/") then
+      check(string.match(url, "^(https?:)")..string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^//") then
+      check(string.match(url, "^(https?:)")..newurl)
+    elseif string.match(newurl, "^\\/") then
+      check(string.match(url, "^(https?://[^/]+)")..string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^/") then
+      check(string.match(url, "^(https?://[^/]+)")..newurl)
+    end
+  end
+
+  local function checknewshorturl(newurl)
+    if string.match(newurl, "^%?") then
+      check(string.match(url, "^(https?://[^%?]+)")..newurl)
+    elseif not (string.match(newurl, "^https?:\\?/\\?//?/?")
+       or string.match(newurl, "^[/\\]")
+       or string.match(newurl, "^[jJ]ava[sS]cript:")
+       or string.match(newurl, "^[mM]ail[tT]o:")
+       or string.match(newurl, "^vine:")
+       or string.match(newurl, "^android%-app:")
+       or string.match(newurl, "^%${")) then
+      check(string.match(url, "^(https?://.+/)")..newurl)
+    end
+  end
+
+  local function adduser(userurl)
+    disco_users[string.match(userurl, "([^/]+)$")] = true
+  end
+
+  local function addtags(comment)
+    for tag in string.gmatch(comment, "#([a-zA-Z][0-9a-zA-Z%-_%.]*)") do
+      disco_tags[tag] = true
+    end
+  end
+
+  local function pagination(html, start, middle, end_, offset, limit, key)
+    local data = load_json_file(html)
+    offset = tonumber(offset)
+    for k, v in pairs(data[key]) do
+      offset = offset + 1
+      adduser(v["user"]["full_url"])
+      if key == "comments" then
+        addtags(v["body"])
+      end
+    end
+    check(start .. tostring(offset) .. middle .. limit .. end_)
+  end
+
+  if string.match(url, "^https?://api%.vid%.me/") then
+    check(string.match(url, "^(https?://)") .. "vid.me/api" .. string.match(url, "^https?://[^/]*(/.+)$"))
+  end
+
+  if string.match(url, "^https://vid%.me/api/") then
+    check(string.match(url, "^(https?://)") .. "api.vid.me" .. string.match(url, "^https?://[^/]*/api(/.+)$"))
+  end
+
+  if allowed(url, nil) and not string.match(url, "^https?://[^/]*cloudfront%.net") then
+    html = read_file(file)
+
+    if string.match(url, "^https?://vid%.me/e/[0-9a-zA-Z]+") then
+      base = string.match(url, "^(https?://vid%.me/e/[0-9a-zA-Z]+)")
+      check(base)
+      check(base .. "?loop=1")
+      check(base .. "?muted=1")
+      check(base .. "?stats=1")
+      check(base .. "?loop=1&muted=1")
+      check(base .. "?muted=1&stats=1")
+      check(base .. "?loop=1&stats=1")
+      check(base .. "?loop=1&muted=1&stats=1")
+      -- extra
+      check(base .. "?muted=1&loop=1")
+      check(base .. "?stats=1&loop=1")
+      check(base .. "?stats=1&muted=1")
+      check(base .. "?loop=1&stats=1&muted=1")
+      check(base .. "?muted=1&loop=1&stats=1")
+      check(base .. "?muted=1&stats=1&loop=1")
+      check(base .. "?stats=1&loop=1&muted=1")
+      check(base .. "?stats=1&muted=1&loop=1")
+    end
+
+    if string.match(url, "^https?://api%.vid%.me/video/[0-9]+$") then
+      local data = load_json_file(html)
+      if data["status"] == true then
+        identifiers[string.match(data["video"]["full_url"], "([0-9a-zA-Z]+)$")] = true
+        adduser(data["video"]["user"]["full_url"])
+        addtags(data["video"]["description"])
+        check(string.gsub(data["video"]["thumbnail_ai"], "{}", "120x"))
+        check(string.gsub(data["video"]["thumbnail_ai"], "{}", "405x"))
+        check(string.gsub(data["video"]["thumbnail_ai"], "{}", "600x"))
+      end
+    end
+
+    if string.match(url, "^https?://api%.vid%.me/video/[0-9]+/likes%?offset=[0-9]+&limit=[0-9]+$") then
+      local start, offset, middle, limit = string.match(url, "^(https?://api%.vid%.me/video/[0-9]+/likes%?offset=)([0-9]+)(&limit=)([0-9]+)$")
+      pagination(html, start, middle, "", offset, limit, "votes")
+    end
+
+    if string.match(url, "^https?://api%.vid%.me/video/[0-9]+/comments%?offsetAtParentLevel=true&order=score&offset=[0-9]+&limit=[0-9]+$") then
+      local start, offset, middle, limit = string.match(url, "^(https?://api%.vid%.me/video/[0-9]+/comments%?offsetAtParentLevel=true&order=score&offset=)([0-9]+)(&limit=)([0-9]+)$")
+      pagination(html, start, middle, "", offset, limit, "comments")
+    end
+
+    for newurl in string.gmatch(html, '([^"]+)') do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "([^']+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, ">%s*([^<%s]+)") do
+       checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "[^%-]href='([^']+)'") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, '[^%-]href="([^"]+)"') do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, ":%s*url%(([^%)]+)%)") do
+      check(newurl)
+    end
+  end
+
+  return urls
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  status_code = http_stat["statcode"]
+
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. "  \n")
+  io.stdout:flush()
+
+  if (status_code >= 200 and status_code <= 399) then
+    downloaded[url["url"]] = true
+    downloaded[string.gsub(url["url"], "https?://", "http://")] = true
+  end
+
+  if abortgrab == true then
+    io.stdout:write("ABORTING...\n")
+    return wget.actions.ABORT
+  end
+  
+  if status_code >= 500 or
+    (status_code >= 400 and status_code ~= 404 and status_code ~= 403) or
+    status_code == 0 then
+    io.stdout:write("Server returned "..http_stat.statcode.." ("..err.."). Sleeping.\n")
+    io.stdout:flush()
+    os.execute("sleep 1")
+    tries = tries + 1
+    if tries >= 5 then
+      io.stdout:write("\nI give up...\n")
+      io.stdout:flush()
+      tries = 0
+      if allowed(url["url"], nil) or status_code == 500 then
+        return wget.actions.ABORT
+      else
+        return wget.actions.EXIT
+      end
+    else
+      return wget.actions.CONTINUE
+    end
+  end
+
+  tries = 0
+
+  local sleep_time = 0
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  return wget.actions.NOTHING
+end
+
+wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total_downloaded_bytes, total_download_time)
+  local file = io.open(item_dir..'/'..warc_file_base..'_data.txt', 'w')
+  for user, _ in pairs(disco_users) do
+    file:write("user:" .. user .. "\n")
+  end
+  for tag, _ in pairs(disco_tags) do
+    file:write("tag:" .. tag .. "\n")
+  end
+  file:close()
+end
+
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if abortgrab == true then
+    return wget.exits.IO_FAIL
+  end
+  return exit_status
+end
